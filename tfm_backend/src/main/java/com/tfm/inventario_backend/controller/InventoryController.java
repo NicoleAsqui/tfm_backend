e24b430449b4f4f62339169b48e5c649e25395bc
Nicole Asqui <nasqui@espol.edu.ec>
2025-07-02 02:08:35 -0500

    fix: cors error

--- tfm_backend/src/main/java/com/tfm/inventario_backend/controller/InventoryController.java
+++ tfm_backend/src/main/java/com/tfm/inventario_backend/controller/InventoryController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
